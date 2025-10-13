83046567124c16c0d43ace701a60183be8b9b088
jelle-westra <jelwestra@gmail.com>
2025-10-13 10:31:51 +0200

    added the femto project

---
+++ content/4-femto/js/custom_TRvG0OIj.js
@@ -0,0 +1,7 @@
+/* Slide 5 (#34) */
+var sidebarTrigger = document.getElementById("sidebarTrigger");
+var lmbtn = document.getElementById("lmbtn");
+
+lmbtn.onclick = function() {
+  sidebarTrigger.click();
+}
